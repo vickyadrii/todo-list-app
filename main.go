4dc5ef145d00344772c221dacb20cfd27e64c754
vickyadri29 <vickyadri290501@gmail.com>
2023-10-14 16:57:15 +0800

    test deployment

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"os"
 	"todo-list-app/config"
 	"todo-list-app/routes"
 
 	"github.com/labstack/echo/v4"
 )
 
+func getPort() string {
+	port := os.Getenv("APPPORT")
+	return port
+}
+
 func main() {
 	// config.LoadEnv()
 	config.InitDB()
 	e := echo.New()
 	routes.TodoListRoutes(e)
-	e.Logger.Fatal(e.Start(fmt.Sprintf(":%v", 8000)))
+	e.Logger.Fatal(e.Start(":" + getPort()))
 }
